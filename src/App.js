21e0d465a26f5abc5b8d1b70878f09b1b3df7e2a
Dalping <74459925+Bokyung626@users.noreply.github.com>
2021-08-23 02:50:48 +0900

    use react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import './App.css';
 import Header from './components/Header';
 import DayList from './components/DayList';
 import Day from './components/Day';
+import EmptyPage from './components/EmptyPage';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() { //함수형 컴포넌트 
   return (
     <BrowserRouter>
-    <div className="App">
-      <Header />
-        <switch> 
-          <Route path="/">
-            <DayList />
-          </Route>
-          <Route path="/day">
-            <Day/>
-          </Route>
-        </switch>
-    </div>
+      <div className="App">
+        <Header />
+          <Switch> 
+            <Route exact path="/"> 
+              <DayList />
+            </Route>
+            <Route path="/day/:day">
+              <Day/>
+            </Route>
+            <Route>
+              <EmptyPage/>
+            </Route>
+          </Switch>
+      </div>
     </BrowserRouter>
   );
 }
--- src/components/Day.js
+++ src/components/Day.js
@@ -1,22 +1,25 @@
 import dummy from "../db/data.json";
+import { useParams } from "react-router";
 
 export default function Day(){
-
-    const day = 3;
+    const day = useParams().day;
     const wordList = dummy.words.filter(word=>(
-        word.day === day
+        word.day === Number(day)
     ));
 
     return(
-        <table>
-            <tbody>
-                {wordList.map(word=>(
-                    <tr key={word.id}>
-                        <td>{word.eng}</td>
-                        <td>{word.kor}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        <>
+            <h2>Day {day}</h2>
+            <table>
+                <tbody>
+                    {wordList.map(word=>(
+                        <tr key={word.id}>
+                            <td>{word.eng}</td>
+                            <td>{word.kor}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
     );
 }

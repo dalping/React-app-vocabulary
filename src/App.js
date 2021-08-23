811913c2e5c3c04f233ea3b7beb52230090fe465
Dalping <74459925+Bokyung626@users.noreply.github.com>
2021-08-23 19:21:14 +0900

    CRUD complete

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Header from './components/Header';
 import DayList from './components/DayList';
 import Day from './components/Day';
 import EmptyPage from './components/EmptyPage';
+import CreateWord from './components/CreateWord';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import CreateDay from './components/CreatDay';
 
 function App() { //함수형 컴포넌트 
   return (
@@ -16,6 +18,12 @@ function App() { //함수형 컴포넌트
             </Route>
             <Route path="/day/:day">
               <Day/>
+            </Route>
+            <Route path="/create_word">
+              <CreateWord/>
+            </Route>
+            <Route path="/create_day">
+              <CreateDay/>
             </Route>
             <Route>
               <EmptyPage/>
--- src/components/Day.js
+++ src/components/Day.js
@@ -1,22 +1,19 @@
-import dummy from "../db/data.json";
 import { useParams } from "react-router";
+import Word from "./Word";
+import useFetch from "../hooks/useFetch";
 
 export default function Day(){
-    const day = useParams().day;
-    const wordList = dummy.words.filter(word=>(
-        word.day === Number(day)
-    ));
+    const {day} = useParams(); // Route path="/day/:day"로 넘겨받은 객체의 첫번째 인자를 넘겨받음
+
+    const words = useFetch(`http://localhost:3001/words?day=${day}`);
 
     return(
         <>
             <h2>Day {day}</h2>
             <table>
                 <tbody>
-                    {wordList.map(word=>(
-                        <tr key={word.id}>
-                            <td>{word.eng}</td>
-                            <td>{word.kor}</td>
-                        </tr>
+                    {words.map(word=>(
+                        <Word word={word} key={word.id}/>
                     ))}
                 </tbody>
             </table>
